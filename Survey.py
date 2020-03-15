f4bc1869370d6fab49406c4410b9a17d908d4bf6
ChloeQuijano <61916412+ChloeQuijano@users.noreply.github.com>
2020-03-14 20:31:59 -0400

    Add files via upload

--- Survey.py
+++ Survey.py
@@ -10,13 +10,15 @@ options_list = [
     [""]]
 
 #user input of answers
-answer_list = []
+answer_list1 = []
+answer_list2 = []
 temp_list = []
 answer = 0
 remove_index = []
 
 # question 1
-def q_1A():
+
+def q_1A(): # Question 1 First Choice
     print(questions_list[0] + "\n" + "\n".join(options_list[0]))
     print("What is your first choice?")
     ans = input()
@@ -25,12 +27,12 @@ def q_1A():
         print("Invalid response. Type a number from 1 to 7")
         q_1A()
     else:
-        answer_list.append(options_list[0][answer-1])
+        answer_list1.append(options_list[0][answer-1])
         remove_index.append(answer)
         temp_list = options_list[0][:answer-1] + options_list[0][answer:]
         q_1B(options_list, temp_list, remove_index)
 
-def q_1B(options_list, temp_list, remove_index):
+def q_1B(options_list, temp_list, remove_index): # Question 1 Second Choice
     print("What is your second choice?" + "\n" + "\n".join(temp_list))
     ans = input()
     answer = int(ans)
@@ -41,18 +43,57 @@ def q_1B(options_list, temp_list, remove_index):
         print("Invalid Response. Choose one of the following:" + "\n")
         q_1B(options_list, temp_list, remove_index)
     else:
-        answer_list.append(options_list[0][answer-1]) # put answer into answer list
-        print(answer_list)
-        temp_index = temp_list.index(answer_list[1]) # remove that answer from the temp list by getting its index in temp list
-        print(temp_index)
-        remove_index.append(answer) # put index of removed - index is in original list
-        print(temp_list)
+        answer_list1.append(options_list[0][answer-1]) # put answer into answer list
+        temp_index = temp_list.index(answer_list1[1]) # remove that answer from the temp list by getting its index in temp list
+        remove_index.append(answer) # put index of removed - index is in original list -1
         del temp_list[temp_index]
-        print(temp_list)
         q_1C(options_list, temp_list, remove_index)
 
-def q_1C(options_list, temp_list, remove_index):
+def q_1C(options_list, temp_list, remove_index): # Question 1 Third Choice
+    print("What is your third choice?" + "\n" + "\n".join(temp_list))
+    ans = input()
+    answer = int(ans)
+    if answer == remove_index[0] or answer == remove_index[1]:
+        print("Invalid input. Choose one of the following:" + "\n")
+        q_1C(options_list, temp_list, remove_index)
+    elif answer <= 0 or answer > 7:
+        print("Invalid Response. Choose one of the following:" + "\n")
+        q_1C(options_list, temp_list, remove_index)
+    else:
+        answer_list1.append(options_list[0][answer-1]) # put answer into answer list
+        remove_index.append(answer) # put number of removed - index is -1
+        temp_list = []
+        answer = 0
+
+def q_2():
+    print(questions_list[1] + "\n" + "\n".join(options_list[1]))
+
+    print("You may select up to 5 (Enter 'done' when you no longer want to select):")
+    enter = True
+    i = 0
+    x = {1: "first", 2: "second", 3: "third", 4: "fourth", 5: "fifth"}
+    while enter:
+        if i == 5:
+            enter = False
+            print("Max count reached")
+            break
+        else:
+            print("Select your " + x[i+1] + " choice:")
+            ans = input()
+            if ans == "done":
+                break
+            answer = int(ans)
+            if answer <= 0 or answer > 7:
+                print("Invalid input. Please choose one of the following:")
+                q_2()
+            else:
+                answer_list2.append(options_list[1][answer-1])
+                i += 1
     
+    print(answer_list2)
+
+
 
 if __name__=="__main__":
     q_1A()
+    q_2()
